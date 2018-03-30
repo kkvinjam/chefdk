564ff75f92c46996c9a08342034ad0a3055151bd
Kishore Vinjam <kkvinjam@gmail.com>
2018-03-30 01:40:47 -0700

    linux cookbook

---
+++ cookbooks/lab-linux/recipes/apace.rb
@@ -0,0 +1,5 @@
+# Install the apache webserver
+
+package "httpd" do
+  action :Install
+end
---
+++ roles/lab-linux.rb
@@ -0,0 +1,3 @@
+name "lab-linux"
+description "role for Package install in Linux Servers"
+run_list "recipe[lab-linux::apache]"
